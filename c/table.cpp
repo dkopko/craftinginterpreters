720cd73184d73d6cb7870b68d8cc6131d925c503
Daniel Kopko <dkopko@runbox.com>
2019-05-31 07:41:05 -0400

    Notes on tableRemoveWhite() changes needed for tri-table transition.

--- c/table.cpp
+++ c/table.cpp
@@ -229,15 +229,74 @@ done:
 
 //CBINT FIXME Redundant
 void
-tableRemoveWhite(Table *table)  //really removeWhiteKeys()
+tableRemoveWhite(Table *table)  //really removeStringsTableWhiteKeys()
 {
+  // There are several issues going on here:
+  // 1) This function was only ever used on vm.strings, but was written
+  //    generally.  Should we maintain the general case in the rewrite?
+  // 2) Table deletions were true deletions, whereas tri-table deletions are
+  //    the writing of TOMBSTONEs under the given key (to mask older values in
+  //    the earlier parts of the tri-table).  Therefore, deleted keys cannot
+  //    truly be collected (though their values can be).
+  // 3) Strings are no longer being exclusively interned.  It used to be the
+  //    case that the table was queried directly when interning a given string
+  //    described by a char *.  Now, a temporary ObjString key is generated to
+  //    query the tri-table for the given key when interning, which means that
+  //    for a little while there will be two ObjStrings with the same contents.
+  //    Such temporary ObjStrings may ultimately occur at other times as well.
+  // 4) In the old case, removal of white keys of vm.strings means that these
+  //    strings occur no where in the rest of the state of the VM (or else they
+  //    would have been visited and marked).  If we presume that any new
+  //    occurence of this string would force a new entry into the A table of
+  //    vm.strings, can we get away with just deleting from the A table and
+  //    disregarding any presence in B or C?  Unfortunately, no, as the B and
+  //    C tables need to still be queried at times, and if an entry in these
+  //    tables' structures exists, but the key's memory has been reused, then
+  //    this cannot work.  It would be fine to truly delete from A, but B and
+  //    C cannot be considered modifiable.  This means, in fact, that presence
+  //    of a key in in the B or C table of vm.strings must entail that it gets
+  //    marked as still needed. (Which brings up a side point, which is that
+  //    marking the key Obj itself is a mutation at present, but needs to become
+  //    some external notion of marking.)
+
+  // Solution:
+  // 1) Mark all key Objs in vm.strings sub-tables B and C.
+  // 2) Truly delete (don't write TOMBSTONEs) any keys still existing as white
+  //    in sub-table A of vm.strings.
+
+  //The idea is that any entry whose key is white must necessarily have no users
+  //(or else the key would have been visited and marked).  This can only be
+  //true for ObjStrings if they are interned (as they were in the original code,
+  //but are no longer).
+
   //NOTE!! This was only ever used on the vm.strings table in the original
   // code, meaning that interned strings which were not reached by the garbage
   // collector during its recursive grayObject()ing would then be automatically
   // freed.
+  //
+  // The issue with the new tri-table is that when a key gets "removed' from
+  // the tri-table, it is only in a logical sense in that a TOMBSTONE value
+  // gets written to the A subtable indicating "this key does not exist in
+  // this table" and masking off any values which may have otherwise been
+  // found in the deeper B and C layers.  This means that removing a key must
+  // still keep the key physically around, at least until such time as it
+  // is dropped in generation of a C table.
+  //
+  // So really, what we have to do is...
+  //
+  // 1) Mark things that are still doing their job
+  // 1a) Mark every TOMBSTONE'd key in A which exists as a non-TOMBSTONE in B or C.
+  // 1b) Mark every TOMBSTONE'd key in B which exists as a non-TOMBSTONE in C.
+  //
+  // 2) Be careful that strings are longer being interned (and so can exist with
+  // the same contents in separate locations)
+  //    FIXME how to do this?
+  //
+  //   Mark every key in B which is not in A (including TOMBSTONE'd keys).
+  //   Mark every key in C which is not in A or B, unless it's a TOMBSTONE key.
+  //   For every white key in B which exists in A, we can truly delete it (not just TOMBSTONE it) from B.
+  //   For every key in C which exists in A or B, we can truly delete it (not just TOMBSTONE it) from B.
 
-  //TODO:
-  //  Iterate A?
 #if 0
   for (int i = 0; i <= table->capacityMask; i++) {
     Entry* entry = &table->entries[i];
