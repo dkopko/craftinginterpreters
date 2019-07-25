5ffa1b10e48b72d53dcb9888abb4fea98a81b151
Daniel Kopko <dkopko@runbox.com>
2019-07-24 23:11:01 -0400

    Fleshed out printStateOfWorld().

--- c/memory.cpp
+++ c/memory.cpp
@@ -539,12 +539,51 @@ void printStateOfWorld(const char *desc) {
   printf("----- end vm.globals -----\n");
 
   printf("----- begin vm.tristack -----\n");
+  for (unsigned int i = 0, e = vm.tristack.stackDepth; i < e; ++i) {
+    const char *regionName;
+    if (i >= vm.tristack.cbi && i < vm.tristack.bbi) {
+      regionName = "C";
+    } else if (i >= vm.tristack.bbi && i < vm.tristack.abi) {
+      regionName = "B";
+    } else {
+      regionName = "A";
+    }
+    printf("tristack[%d](%s) ", i, regionName);
+    printValue(*tristack_at(&vm.tristack, i));
+    printf("\n");
+  }
   printf("----- end vm.tristack -----\n");
 
   printf("----- begin vm.triframes -----\n");
+  for (unsigned int i = 0, e = vm.triframes.frameCount; i < e; ++i) {
+    const char *regionName;
+    if (i >= vm.triframes.cbi && i < vm.triframes.bbi) {
+      regionName = "C";
+    } else if (i >= vm.triframes.bbi && i < vm.triframes.abi) {
+      regionName = "B";
+    } else {
+      regionName = "A";
+    }
+    printf("triframes[%d](%s) ", i, regionName);
+    CallFrame *cf = triframes_at(&vm.triframes, i);
+    printObject(cf->closure.id(), cf->closure.co(), (const Obj *)cf->closure.clip());
+    printf(" (slotsIndex: %u, slotsCount: %u)\n", cf->slotsIndex, cf->slotsCount);
+    for (unsigned int j = 0, f = cf->slotsCount; j < f ; ++j) {
+      printf(" stack[%u]: ", j + cf->slotsIndex);
+      printValue(cf->slots[j]);
+      printf("\n");
+    }
+    printf("\n");
+  }
   printf("----- end vm.triframes -----\n");
 
   printf("----- begin vm.openUpvalues -----\n");
+  {
+    OID<ObjUpvalue> it = vm.openUpvalues;
+    printObject(it.id(), it.co(), (const Obj *)it.clip());
+    printf("\n");
+    it = it.clip()->next;
+  }
   printf("----- end vm.openUpvalues -----\n");
 
   printf("===== END STATE OF WORLD %s =====\n", desc);
